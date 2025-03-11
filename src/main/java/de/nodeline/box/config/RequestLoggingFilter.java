b8ba05e2ed8b5a6c4b01e509a5e3f0c45bedcc47
Florian Patzer <florianpatzer42@gmail.com>
2025-03-11 17:10:32 +0100

    added sysout logging for request origin

---
+++ src/main/java/de/nodeline/box/config/RequestLoggingFilter.java
@@ -0,0 +1,22 @@
+package de.nodeline.box.config;
+
+import org.springframework.stereotype.Component;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Component
+public class RequestLoggingFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        System.out.println("Incoming request from origin: " + req.getHeader("Origin"));
+        chain.doFilter(request, response);
+    }
+}
